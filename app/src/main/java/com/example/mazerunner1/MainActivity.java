f0868672ed65335de80e4a91801b087c288189fb
Blake Hawkins <hawkinsba@hendrix.edu>
2019-04-15 22:40:06 -0500

    Update user interface
    Fixed the user interface of MainActivity to match the changes that we decided to make

--- app/src/main/java/com/example/mazerunner1/MainActivity.java
+++ app/src/main/java/com/example/mazerunner1/MainActivity.java
@@ -6,7 +6,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.RadioButton;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -19,36 +19,24 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void initUIComponents() {
-        View difficultyView = getLayoutInflater().inflate(R.layout.dialog_difficulty, null);
-        AlertDialog.Builder difficultyBuilder = new AlertDialog.Builder(MainActivity.this);
-        difficultyBuilder.setView(difficultyView);
-        final AlertDialog difficultySelection = difficultyBuilder.create();
-
         View dimensionsView = getLayoutInflater().inflate(R.layout.dialog_dimensions, null);
-        AlertDialog.Builder dimensionsBuilder = new AlertDialog.Builder(MainActivity.this);
-        dimensionsBuilder.setView(dimensionsView);
-        final AlertDialog dimensionsDialog = dimensionsBuilder.create();
+        final AlertDialog dimensionsDialog = buildDialog(R.layout.dialog_dimensions);
 
         View tutorialView = getLayoutInflater().inflate(R.layout.dialog_tutorial, null);
-        AlertDialog.Builder tutorialBuilder = new AlertDialog.Builder(MainActivity.this);
-        tutorialBuilder.setView(tutorialView);
-        final AlertDialog tutorialDialog = tutorialBuilder.create();
+        final AlertDialog tutorialDialog = buildDialog(R.layout.dialog_tutorial);
 
         Button play = findViewById(R.id.play);
         Button create = findViewById(R.id.confirmDimensions);
-        Button load = findViewById(R.id.load);
         Button tutorial = findViewById(R.id.tutorial);
         Button confirmDimensions = dimensionsView.findViewById(R.id.confirmDimensions);
-        Button confirmDifficulty = difficultyView.findViewById(R.id.confirmDifficulty);
-        final RadioButton easy = difficultyView.findViewById(R.id.easy);
-        final RadioButton medium = difficultyView.findViewById(R.id.medium);
-        final RadioButton hard = difficultyView.findViewById(R.id.hard);
-        final RadioButton random = difficultyView.findViewById(R.id.random);
+        Button exitTutorial = tutorialView.findViewById(R.id.exitTutorial);
+        final EditText width = dimensionsView.findViewById(R.id.width);
+        final EditText height = dimensionsView.findViewById(R.id.height);
 
         play.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                difficultySelection.show();
+                //TODO: Start custom maze selection activity.
             }
         });
 
@@ -59,13 +47,6 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-        load.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-
-            }
-        });
-
         tutorial.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -73,18 +54,28 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        exitTutorial.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                tutorialDialog.dismiss();
+            }
+        });
+
         confirmDimensions.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
+                Intent createIntent = new Intent(MainActivity.this, CreateActivity.class);
+                createIntent.putExtra("WIDTH", width.getText());
+                createIntent.putExtra("HEIGHT", height.getText());
+                startActivity(createIntent);
             }
         });
+    }
 
-        confirmDifficulty.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-
-            }
-        });
+    private AlertDialog buildDialog(int resource) {
+        View mView = getLayoutInflater().inflate(resource, null);
+        AlertDialog.Builder mBuilder = new AlertDialog.Builder(MainActivity.this);
+        mBuilder.setView(mView);
+        return mBuilder.create();
     }
 }
