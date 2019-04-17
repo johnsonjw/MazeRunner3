c5457a476cbcdd1c4fb0ea85d1b0317fe6494be8
theDrunkenManatee <theDrunkenManatee@users.noreply.github.com>
2019-04-16 21:48:02 -0500

    methods added

--- app/src/main/java/com/example/mazerunner1/rendering/MazeRenderer.java
+++ app/src/main/java/com/example/mazerunner1/rendering/MazeRenderer.java
@@ -1,4 +1,13 @@
 package com.example.mazerunner1.rendering;
 
 public class MazeRenderer {
+
+    public MazeRenderer() {
+
+    }
+
+    //TODO
+    public String getTextScreen() {
+        return "MAZE NOT RENDERED";
+    }
 }
