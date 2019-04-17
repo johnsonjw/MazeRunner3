8317bcebf3feb6ce8378f27127750dfbda009168
theDrunkenManatee <theDrunkenManatee@users.noreply.github.com>
2019-04-16 22:03:34 -0500

    methods added

---
+++ app/src/main/java/com/example/mazerunner1/Maze.java
@@ -0,0 +1,23 @@
+package com.example.mazerunner1;
+
+public class Maze {
+    char[][] tileChars;
+    public Maze(char[][] tilesIn) {
+        tileChars = tilesIn;
+    }
+
+    public char getTileAt(int x, int y) {
+        return tileChars[x][y];
+    }
+
+    public String toString() {
+        String out="";
+        for(int x=0; x<tileChars.length;x++) {
+            for(int y=0; y<tileChars[x].length;y++) {
+                out+=tileChars[x][y];
+            }
+            out +="\n";
+        }
+        return out;
+    }
+}
--- app/src/main/java/com/example/mazerunner1/rendering/MazeRenderer.java
+++ app/src/main/java/com/example/mazerunner1/rendering/MazeRenderer.java
@@ -6,6 +6,12 @@ public class MazeRenderer {
 
     }
 
+    public void setRenderPoint(double x, double y) {
+
+    }
+
+
+
     //TODO
     public String getTextScreen() {
         return "MAZE NOT RENDERED";
