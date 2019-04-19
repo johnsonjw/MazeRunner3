b7abcf8c94e8884c8bea3d104cea923d38c12cd4
johnsonjw <46804126+johnsonjw@users.noreply.github.com>
2019-04-19 08:49:52 -0500

    Maze Creation fixes
    Trying to find a way to make mazes a variable size without breaking everything

---
+++ app/src/main/java/com/example/mazerunner1/Maze_Creation.java
@@ -0,0 +1,25 @@
+package com.example.mazerunner1;
+
+
+public class Maze_Creation {
+
+    public char[][] mazeGrid;
+    public int width;
+    public int height;
+
+   /* public String toString() {
+        String out = "";
+        for (int x = 0; x < mazeGrid[0].length; x++) {
+            for (int y = 0; y < mazeGrid[1].length; y++) {
+                mazeGrid[x][y] = '#';
+
+            }
+            if ()
+        }
+        return  out;
+    }*/ 
+}
+
+
+
+
