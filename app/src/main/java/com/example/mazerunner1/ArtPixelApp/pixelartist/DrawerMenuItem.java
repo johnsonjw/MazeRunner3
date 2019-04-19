269078043c8e7354c62187aca43847c2052ae0f3
johnsonjw <46804126+johnsonjw@users.noreply.github.com>
2019-04-18 21:46:00 -0500

    Added Pixel Art Editor
    Third party Code curtesy of  github

---
+++ app/src/main/java/com/example/mazerunner1/ArtPixelApp/pixelartist/DrawerMenuItem.java
@@ -0,0 +1,23 @@
+/*
+package com.example.mazerunner1.ArtPixelApp.pixelartist;
+
+public abstract class DrawerMenuItem {
+    private int iconId;
+    private int stringId;
+
+    DrawerMenuItem(int id, int string) {
+        iconId = id;
+        stringId = string;
+    }
+
+    public abstract void execute();
+
+    int getIconId() {
+        return iconId;
+    }
+
+    int getStringId() {
+        return stringId;
+    }
+}
+*/
