9c375ca512461f0ba082bf39b05bc48d67d14da9
johnsonjw <46804126+johnsonjw@users.noreply.github.com>
2019-04-28 16:57:23 -0500

    Added spinner  as well as fixing create maze
    Trying to restart my android studio

--- app/src/main/java/com/example/mazerunner1/Maze_Creation.java
+++ app/src/main/java/com/example/mazerunner1/Maze_Creation.java
@@ -1,10 +1,13 @@
-//package com.example.mazerunner1;
+/*
+package com.example.mazerunner1;
 
 
-/*public class Maze_Creation extends  AppCompatActivity implements OnClickListener {
+public class Maze_Creation extends  AppCompatActivity implements OnClickListener {
     private LinearLayout parent;
     private Button b1;
     private Button b2;
+    public int inputX;
+    public int inputY;
     public int testX = 10;
     public int testY = 10;
     public String[] buttonName = ["b1","b2","b3","b4","b5","b6","b7","b8","b9","b10"];
@@ -16,13 +19,20 @@
     //ToDo Generate dynamic based on the input from the user
     protected void onCreate(Bundle  savedInstanceState){
         super.onCreate(savedInstanceState);
-        setContentview(R.layout.create);
+        setContentview(R.layout.activity_create);
 
-        parent= (LinearLayout)findViewById(R.id.create);
+        createSpinner();
+        parent= (LinearLayout)findViewById(R.id.activity_create);
         createDynamicButtons(testX,testY);
     }
+    public static void GetXandY(int x,int y){
+        inputX = x;
+        inputY = y;
+    }
 
-    public void createDynamicButtons(int x, int y){
+
+
+    public void createDynamicButtons(){
         for (i=0;i<x;i++){
             for (b=0;b<y;b++){
                 b1= new Button(create.this);
@@ -32,9 +42,17 @@
                 b1.setText()
             }
         }
-    }*/
+    }
+    public void createSpinner(){
+        Spinner spinner = findViewById(R.id.userSpinner);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.mapIcons,android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
+        spinner.setOnItemSelectedListener(this);
 
-   /* public String toString() {
+    }
+   */
+/* public String toString() {
         String out = "";
         for (int x = 0; x < mazeGrid[0].length; x++) {
             for (int y = 0; y < mazeGrid[1].length; y++) {
@@ -44,10 +62,12 @@
             if ()
         }
         return  out;
-    }*/
+    }*//*
+
 
 //}
 
 
 
 
+*/
