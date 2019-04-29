a68ecacf5f2a0b2acb4df967b40bf5d0634cc3bb
johnsonjw <46804126+johnsonjw@users.noreply.github.com>
2019-04-28 22:08:38 -0500

    Fixing more stuffs

--- app/src/main/java/com/example/mazerunner1/Maze_Creation.java
+++ app/src/main/java/com/example/mazerunner1/Maze_Creation.java
@@ -1,8 +1,20 @@
-/*
+
 package com.example.mazerunner1;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.Spinner;
+import android.widget.Toast;
 
 
-public class Maze_Creation extends  AppCompatActivity implements OnClickListener {
+public class Maze_Creation extends  AppCompatActivity implements AdapterView.OnItemSelectedListener  {
     private LinearLayout parent;
     private Button b1;
     private Button b2;
@@ -10,48 +22,59 @@ public class Maze_Creation extends  AppCompatActivity implements OnClickListener
     public int inputY;
     public int testX = 10;
     public int testY = 10;
-    public String[] buttonName = ["b1","b2","b3","b4","b5","b6","b7","b8","b9","b10"];
+  //  public String[] buttonName = ["b1","b2","b3","b4","b5","b6","b7","b8","b9","b10"]
     public String[] Maze;
 
 
-
-
     //ToDo Generate dynamic based on the input from the user
-    protected void onCreate(Bundle  savedInstanceState){
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentview(R.layout.activity_create);
+        setContentView(R.layout.activity_create);
 
         createSpinner();
-        parent= (LinearLayout)findViewById(R.id.activity_create);
-        createDynamicButtons(testX,testY);
+
+        createDynamicButtons(testX, testY);
     }
-    public static void GetXandY(int x,int y){
+
+    public void GetXandY(int x, int y) {
         inputX = x;
         inputY = y;
     }
 
 
-
-    public void createDynamicButtons(){
-        for (i=0;i<x;i++){
-            for (b=0;b<y;b++){
-                b1= new Button(create.this);
-                b2 = new Button(create.this);
-                b1.setId(i+1);
-                b2.setId(b+1);
-                b1.setText()
+    public void createDynamicButtons(int x,int y) {
+        for (int i = 0; i < x; i++) {
+            for (int b = 0; b < y; b++) {
+               //b1 = new Button();
+                // b2 = new Button(create.this);
+                b1.setId(i + 1);
+                b2.setId(b + 1);
+                b1.setText("");
             }
         }
     }
-    public void createSpinner(){
+
+    public void createSpinner() {
         Spinner spinner = findViewById(R.id.userSpinner);
-        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.mapIcons,android.R.layout.simple_spinner_item);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.mapIcons, android.R.layout.simple_spinner_item);
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         spinner.setAdapter(adapter);
         spinner.setOnItemSelectedListener(this);
 
     }
-   */
+
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+        //String text = parent.getItemIdAtPosition(position).toString();
+        //Toast.makeText(parent.getContext(),text,Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> parent) {
+
+    }
+
+
 /* public String toString() {
         String out = "";
         for (int x = 0; x < mazeGrid[0].length; x++) {
@@ -62,12 +85,11 @@ public class Maze_Creation extends  AppCompatActivity implements OnClickListener
             if ()
         }
         return  out;
-    }*//*
+    }*/
 
 
 //}
+}
 
 
 
-
-*/
