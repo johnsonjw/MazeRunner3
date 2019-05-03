63c6e50bbd11a2950c8f9003707671cc3785e193
Blake Hawkins <hawkinsba@hendrix.edu>
2019-05-03 14:33:49 -0500

    Created assets folder with test maze

--- app/src/main/java/com/example/mazerunner1/MazeSelectionActivity.java
+++ app/src/main/java/com/example/mazerunner1/MazeSelectionActivity.java
@@ -11,12 +11,13 @@ import android.widget.ListView;
 import android.widget.TextView;
 
 import java.io.File;
+import java.io.IOException;
 
 public class MazeSelectionActivity extends AppCompatActivity {
     private static final String TAG = "MazeSelectionActivity";
     private ListView mazes;
     private TextView emptyText;
-    private File[] mazeFiles;
+    private String[] files;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,21 +33,26 @@ public class MazeSelectionActivity extends AppCompatActivity {
     }
 
     private void initListItems() {
-        File mazeFolder = getFilesDir();
-        mazeFiles = mazeFolder.listFiles();
-        String[] mazeFileNames = new String[mazeFiles.length];
-        for (int i = 0; i < mazeFileNames.length; i++) {
-            mazeFileNames[i] = mazeFiles[i].getName();
-            Log.d(TAG, "initListItems: Adding item to mazeFiles.");
+        try {
+            files = getAssets().list("mazes");
+        } catch (IOException e) {
+            Utilities.notifyException(this, e);
         }
-        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, mazeFileNames);
+        //mazeFiles = mazeFolder.listFiles();
+        //String[] mazeFileNames = new String[mazeFiles.length];
+        //for (int i = 0; i < mazeFileNames.length; i++) {
+        //    mazeFileNames[i] = mazeFiles[i].getName();
+        //    Log.d(TAG, "initListItems: Adding item to mazeFiles.");
+        //}
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, files);
         if (adapter.getCount() == 0) mazes.setVisibility(View.GONE);
         mazes.setAdapter(adapter);
         mazes.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
                 Intent toGame = new Intent(MazeSelectionActivity.this, GameActivity.class);
-                toGame.putExtra("MAZE_FILE", mazeFiles[i]);
+                toGame.putExtra("MAZE_FILE", files[i]);
+                startActivity(toGame);
             }
         });
     }
